a60fad0ba47ba574b897ae9e6426658cb199ec7c
newstas <45275123+newstas@users.noreply.github.com>
2023-02-06 03:45:22 +0500

    Update wp-functions-php-my-top.php

--- snippet/wp-functions-php-my-top.php
+++ snippet/wp-functions-php-my-top.php
@@ -4,4 +4,4 @@ remove_action( 'wp_head', 'wlwmanifest_link' );
 // Удаляет информацию о текущей версии WordPress
 remove_action( 'wp_head', 'wp_generator' );
 
-// Проверка http://xmlrpc.epizy.com/
+// Проверка подключения XML-RPC http://xmlrpc.epizy.com/ - WordPress XML-RPC Validation
