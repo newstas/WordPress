df109546680f4f3fa209fcfdbc59ad986a16152b
newstas <45275123+newstas@users.noreply.github.com>
2023-02-06 05:10:17 +0500

    Update functions_php-my-top.php

--- snippet/functions_php-my-top.php
+++ snippet/functions_php-my-top.php
@@ -1,3 +1,17 @@
+// Удаляем лишнее с head 
+// 2.0
+// удаляем ссылки доп. фидов (на рубрики)
+remove_action( 'wp_head', 'feed_links_extra', 3 ); 
+// <link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://example.com/xmlrpc.php?rsd" /> для публикации статей через сторонние сервисы
+remove_action( 'wp_head', 'rsd_link');
+// <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://example.com/wp-includes/wlwmanifest.xml" /> . Используется клиентом Windows Live Writer.
+remove_action( 'wp_head', 'wlwmanifest_link');
+// Убираем версию WordPress
+add_filter('the_generator', '__return_empty_string');
+// 3.0
+// Короткая ссылка - без ЧПУ <link rel='shortlink'
+remove_action( 'wp_head', 'wp_shortlink_wp_head', 10 );
+
 // Удаляем 'wlwmanifest' из head
 remove_action( 'wp_head', 'wlwmanifest_link' );
 
