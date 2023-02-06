c7160c6d86819c3a9c11c9d341e68577c458d9a3
newstas <45275123+newstas@users.noreply.github.com>
2023-02-06 05:14:19 +0500

    Update functions_php-my-top.php

--- snippet/functions_php-my-top.php
+++ snippet/functions_php-my-top.php
@@ -24,3 +24,6 @@ remove_action( 'wp_head', 'wlwmanifest_link' );
 
 // Удаляет информацию о текущей версии WordPress
 remove_action( 'wp_head', 'wp_generator' );
+
+                                   
+                                   
