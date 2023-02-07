6fb10f2c98eddd7cdd4b0711048cf53ae5291d14
newstas <45275123+newstas@users.noreply.github.com>
2023-02-07 21:00:48 +0500

    Update functions_php-list.php

--- snippet/functions_php-list.php
+++ snippet/functions_php-list.php
@@ -126,11 +126,5 @@ remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0); // Убирает с
 remove_action('wp_head', 'feed_links_extra', 3); // Запрещаем вывод RSS фида для записей, тегов, рубрик и т.д. Таким образом, мы запрещаем создавать такие фиды, но тем не менее, они будут доступны, если добавить /feed в конец урла.
 remove_action('wp_head', 'feed_links', 2); // Формально если запретить данное действие, то в блоге не должны выводиться ссылки на основную ленту RSS и на RSS ленту комментариев. А на практике это работать не будет, так как функция wp_head не выводит эти самые ссылки на RSS ленты записей и комментариев, их вывод должен осуществляться вручную в файле header.php
 
-//полное отключение Emoji
-remove_action('wp_head',             'print_emoji_detection_script', 7);
-remove_action('wp_print_styles',     'print_emoji_styles');
-remove_action('admin_print_scripts', 'print_emoji_detection_script');
-remove_action('admin_print_styles',  'print_emoji_styles');
-
 //
 
